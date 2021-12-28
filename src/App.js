26f33d727d687e79173fb76bf4b9d87a3435ae1c
hanum <rntls123@naver.com>
2021-12-28 16:35:49 +0900

    useEffect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((current) => current + 1);
+  console.log("I'm running all the time.")
+  const runOnlyOnce = () => console.log("running~");
+  useEffect(runOnlyOnce, []);
+  // useEffect() makes code executed only once when Component rendered.
+
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-      <Button text="hello" />
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
