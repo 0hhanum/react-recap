ff08b541f71afdb49d57b75e85f745666378ad21
hanum <rntls123@naver.com>
2022-01-02 19:27:42 +0900

    Practice #1 - toDoList using map

--- src/App.js
+++ src/App.js
@@ -14,13 +14,18 @@ function App() {
     }
     setToDos((current) => [toDo, ...toDos]); // unpack is JS
     setToDo("");
-    console.log(toDos);
   }
   return <div>
+    <h1>You have {toDos.length} things to do.</h1>
     <form onSubmit={onSubmit}>
       <input onChange={onChange} value={toDo} type="text" placeholder="Write thing to do" />
       <button>Add</button>
     </form>
+    <hr />
+    <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+
   </div>
 }
 
