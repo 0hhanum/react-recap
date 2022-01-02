dfba251d1b00627111932d9de8724dc2c8fb4a51
hanum <rntls123@naver.com>
2022-01-02 18:53:08 +0900

    unpack in JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Created");
-    return () => console.log("I run when Components destroyed")
-  }, [])
-  // if  deps is empty, it means function run only once when it rendered.
-  // The first arg of useEffect is function. And the function returned from first args(function) run when components destroyed.
-  return <h1>Hello!</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((current) => !current);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((current) => [toDo, ...toDos]); // unpack is JS
+    setToDo("");
+    console.log(toDos);
   }
   return <div>
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    {showing ? <Hello /> : null}
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write thing to do" />
+      <button>Add</button>
+    </form>
   </div>
 }
 
