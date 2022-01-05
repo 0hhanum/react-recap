1084a18f69ffe72221203d71b242fc95475b0c2a
hanum <rntls123@naver.com>
2022-01-05 21:30:43 +0900

    Coin-dollar calculator for recap

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -4,6 +4,11 @@ import { useEffect } from "react/cjs/react.development";
 function CoinTracker() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+    const [selected, setSelected] = useState("");
+    const [dollars, setDollars] = useState("");
+    const [price, setPrice] = useState(0);
+    const [result, setResult] = useState("");
+
 
     useEffect(() => {
         const data = fetch("https://api.coinpaprika.com/v1/tickers")
@@ -13,18 +18,42 @@ function CoinTracker() {
                 setLoading(false);
             });
     }, []);
+
+    const onChange = (event) => {
+        const [coin, price] = event.target.value.split(",");
+        setPrice(price);
+        setSelected(coin);
+        // const selected = coins.find((element) => element.name === coin);
+        // find 이용해서 찾을 수 있지만, 배열 검색을 한 번 더 해야하니 애초에 select 통해 값과 코인명 받음.
+    }
+    const onChangeInput = (event) => {
+        setDollars(event.target.value);
+    }
+    const onClick = () => {
+        setResult(
+            `${dollars}$ = ${dollars / price} ${selected}`
+        )
+    }
     return <div>
-        <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+        <h1>The Coins ! {loading ? null : `(${coins.length})`}</h1>
         {loading ? <strong>Loading...</strong> :
-            <select>
+            <select onChange={onChange} value={selected}>
+                <option>Choose Coin to calculate...</option>
                 {
                     coins.map((coin) =>
-                        <option key={coin.name}>
+                        <option key={coin.id} value={[coin.name, coin.quotes.USD.price]}>
                             {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
                         </option>
                     )}
             </select>}
-
+        {selected
+            ? <div>
+                <h3>How much can you buy some {selected} ?</h3>
+                <input type="number" value={dollars} onChange={onChangeInput} placeholder="Input $$$"></input>
+                <button onClick={onClick}>Calculate!</button>
+                <h3>{result}</h3>
+            </div>
+            : null}
     </div>
 
 }
