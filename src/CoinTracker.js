01291977c98392e709d0fcd061e74ba657702871
hanum <rntls123@naver.com>
2022-01-05 20:51:18 +0900

    Fetch to API and render when it done

---
+++ src/CoinTracker.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+
+function CoinTracker() {
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        const data = fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
+    return <div>
+        <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+        {loading ? <strong>Loading...</strong> :
+            <select>
+                {
+                    coins.map((coin) =>
+                        <option key={coin.name}>
+                            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                        </option>
+                    )}
+            </select>}
+
+    </div>
+
+}
+
+export default CoinTracker;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CoinTracker from './CoinTracker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoinTracker />
   </React.StrictMode>,
   document.getElementById('root')
 );
