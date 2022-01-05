97f9f3ce7a03b1a220c7f51e721efbc805b22d44
hanum <rntls123@naver.com>
2022-01-05 22:03:34 +0900

    Fetch Async Await

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CoinTracker from './CoinTracker';
+import Movie from './Movie';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CoinTracker />
+    <Movie />
   </React.StrictMode>,
   document.getElementById('root')
 );
