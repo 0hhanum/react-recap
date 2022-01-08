60f6cf89437f531ca3f6499028cd15e9b86f5b86
hanum <rntls123@naver.com>
2022-01-08 18:03:36 +0900

    react-router-dom / Router, Switch, Link, Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
 import Movie from "./components/Movie";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 
 const App = () => {
-    return <Home />;
+    return <Router>
+        <Switch>
+            <Route path="/movie">
+                <Detail />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Movie = ({ cover_image, title, summary, genres }) => {
     return <div>
         <img src={cover_image} alt="coverImg" />
-        <h2>{title}</h2>
+        <h2><Link to="/movie">{title}</Link></h2>
         <p>{summary}</p>
         <ul>
             {genres ? genres.map((genre) => <li key={genre}>{genre}</li>) : null}
