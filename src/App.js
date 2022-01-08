36e8a8f0d142ac841bd680bdfd879d76fea6c58a
hanum <rntls123@naver.com>
2022-01-08 17:48:26 +0900

    Devide to router & components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
+import Home from "./routes/Home";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
-        const json = await response.json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, []);
-
-    return <div>
-        {loading
-            ? "Loading ..."
-            : <div>
-                {movies.map((movie) =>
-                    <Movie key={movie.id} cover_image={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />)}}
-            </div>}
-    </div>
+    return <Home />;
 }
 
 export default App;
